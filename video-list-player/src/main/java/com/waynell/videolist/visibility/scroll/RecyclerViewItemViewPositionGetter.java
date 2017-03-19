5ce8abb9cc385dacbb78537cf478ec49970f98c1
Ali Illyas <ali@illyas.net>
2017-03-19 11:22:54 -0500

    calculate on custom view

---
+++ video-list-player/src/main/java/com/waynell/videolist/visibility/scroll/RecyclerViewItemViewPositionGetter.java
@@ -0,0 +1,47 @@
+package com.waynell.videolist.visibility.scroll;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by aillyas on 3/19/17.
+ */
+
+public class RecyclerViewItemViewPositionGetter implements ItemsPositionGetter  {
+
+    private LinearLayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView;
+    private int mViewId;
+
+    public RecyclerViewItemViewPositionGetter(LinearLayoutManager layoutManager, RecyclerView recyclerView, int viewId) {
+        mLayoutManager = layoutManager;
+        mRecyclerView = recyclerView;
+        mViewId = viewId;
+    }
+
+    @Override
+    public View getChildAt(int position) {
+        return mLayoutManager.getChildAt(position).findViewById(mViewId);
+    }
+
+    @Override
+    public int indexOfChild(View view) {
+        return mRecyclerView.indexOfChild(view);
+    }
+
+    @Override
+    public int getChildCount() {
+        return mRecyclerView.getChildCount();
+    }
+
+    @Override
+    public int getLastVisiblePosition() {
+        return mLayoutManager.findLastVisibleItemPosition();
+    }
+
+    @Override
+    public int getFirstVisiblePosition() {
+        return mLayoutManager.findFirstVisibleItemPosition();
+    }
+}
